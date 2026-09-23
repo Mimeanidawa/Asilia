a8b773fb8ee4516295c606aa350fa36b0b4dbd54
luleztech <luleztech@gmail.com>
2026-09-23 11:02:19 -0700

    fix(admin): enforce Kotlin JvmTarget.JVM_17 across subprojects in Gradle

--- administrator/android/build.gradle.kts
+++ administrator/android/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 allprojects {
     repositories {
         google()
@@ -19,6 +22,13 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
